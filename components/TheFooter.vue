f5a4eb44be35a893443560d63862acada67bf909
truongnd2-fabbi <truongnd2@fabbi.io>
2022-11-26 21:28:46 +0700

    scss

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer>Footer</footer>
+  <footer class="l-footer">Footer</footer>
 </template>
 
 <script setup lang="ts"></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,37 @@
-import { Title } from './.nuxt/components.d'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge"],
   app: {
     head: {
-      title: 'ShopChow',
+      title: "ShopChow",
       meta: [
         {
-          name: 'description', content: 'Shop chow'
-        }
+          name: "description",
+          content: "Shop chow",
+        },
       ],
       link: [
         {
-          rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded"
-        }
-      ]
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded",
+        },
+      ],
     },
   },
+  css: ['@/assets/style/style.scss'], 
+  tailwindcss: {
+    cssPath: '@/assets/vendor/tailwind.css',
+  },
   runtimeConfig: {
-    API_KEY: process.env.API_KEY
+    API_KEY: process.env.API_KEY,
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/style/abstracts/_variables.scss";'
+        }
+      }
+    }
   }
 });
