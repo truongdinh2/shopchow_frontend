eee0813d76d63beb674960b514170d893b7c4306
unknown <dinhtruong2808@gmail.com>
2022-12-17 02:16:50 +0700

    create/header-footer/01

---
+++ taiwind.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+      },
+      backgroundColor: {
+        'regal-blue': '#243c5a',
+      }
+    }
+  }
+}
