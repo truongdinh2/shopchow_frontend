7fd520bf926838be7765c66d32245e4fd15cd9df
truong2k1 <dinhtruong2808@gmail.com>
2022-12-23 00:14:32 +0700

    create/social-group

---
+++ utils/index.ts
@@ -0,0 +1,27 @@
+export const baseComponent = [{
+    id: 1,
+    name: 'AppButton',
+    text: [
+        'Thêm class để style btn',
+        'Custom click event'
+    ]
+},
+{
+    id: 2,
+    name: 'AppBadge',
+    text: [
+        'sub: số ở trên',
+        'content: nội dung ở dưới',
+        'slot: slot',
+    ]
+},
+{
+    id: 3,
+    name: 'AppInput',
+    text: [
+        'v-model ',
+        'type: type của input',
+        'placeholder',
+    ]
+},
+]
